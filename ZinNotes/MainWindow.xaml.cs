87a570a60e551137ac353e77f1473711f9cd4e02
zinnomeister <zinnomeister@gmail.com>
2022-10-22 17:50:52 +0100

    Add project files.

---
+++ ZinNotes/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ZinNotes.Models;
+
+namespace ZinNotes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BindingList<NotesModel> _notesData;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ZinNotes/Models/NotesModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZinNotes.Models
+{
+     class NotesModel
+    {
+        public DateTime CreationDateTime { get; set; } = DateTime.Now;
+		private bool _isDone;
+        private string _note;
+
+        public bool IsDone
+		{
+			get { return _isDone; }
+			set { _isDone = value; }
+		}
+
+		public string Note
+		{
+			get { return _note; }
+			set { _note = value; }
+		}
+
+
+	}
+}
