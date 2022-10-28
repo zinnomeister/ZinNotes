5803e7e21b6536be5862f381b3e6a8d1baa907d0
zinnomeister <zinnomeister@gmail.com>
2022-10-28 17:18:41 +0100

    first part

--- ZinNotes/MainWindow.xaml.cs
+++ ZinNotes/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using ZinNotes.Models;
+using Path = System.IO.Path;
 
 namespace ZinNotes
 {
@@ -22,11 +24,17 @@ namespace ZinNotes
     /// </summary>
     public partial class MainWindow : Window
     {
-        private BindingList<NotesModel> _notesData;
+        //private BindingList<NotesModel> _notesData;
+        string categoriesPath = "C:\\Users\\zinno\\OneDrive\\Разработка\\ZinNotes\\ZinNotes\\Categories\\";
+
+        string categoryName = null;
 
         public MainWindow()
         {
             InitializeComponent();
+
+
+
         }
 
         private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -41,7 +49,104 @@ namespace ZinNotes
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            GetCategories();
+
+            //UiFirstButton.Click += UiFirstButton_Click;
+        }
+
+
+
+        public void GetCategories()
+        {
+            List<string> categories = new List<string>(Directory.GetDirectories(categoriesPath));
+
+
+            List<string> catShortNames = new List<string>();
+
+            foreach (string dir in categories)
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(dir);
+                catShortNames.Add(directoryInfo.Name);
+                ListBoxItem newItem = new ListBoxItem()
+                {
+                    Content = directoryInfo.Name
+                };
+                UiCategories.Items.Add(newItem);
+            }
+
+            //Categories.ItemsSource = catShortNames;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
 
         }
+
+        public void Categories_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (UiCategories.SelectedItem == null)
+                return;
+
+            ListBoxItem selectedCategory = (ListBoxItem)UiCategories.SelectedItem;
+
+            categoryName = selectedCategory.Content.ToString();
+
+            GetNoteNames(categoryName);
+        }
+
+        public void GetNoteNames(string selectedCategory)
+        {
+            string fullFilesPath = categoriesPath + selectedCategory;
+            List<string> notesFullNames = new List<string>(Directory.GetFiles(fullFilesPath));
+            List<string> notesShortNames = new List<string>();
+
+            UiNotes.Items.Clear();
+
+            foreach (string file in notesFullNames)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                string noteNameWithExtension = fileInfo.Name;
+                string noteName = Path.GetFileNameWithoutExtension(noteNameWithExtension);
+
+                ListBoxItem newItem = new ListBoxItem()
+                {
+                    Content = noteName
+                };
+                UiNotes.Items.Add(newItem);
+            }
+        }
+
+        private void UiNotes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (UiNotes.SelectedItem == null)
+                return;
+
+            ListBoxItem selectedNote = (ListBoxItem)UiNotes.SelectedItem;
+
+            string noteContent = selectedNote.Content.ToString();
+
+            GetNote(noteContent);
+        }
+
+        private void GetNote(string selectedNote)
+        {
+            UiDetails.Items.Clear();
+            string fullNotePath = categoriesPath + categoryName + "\\" + selectedNote + ".txt";
+
+            string noteText = File.ReadAllText(fullNotePath);
+
+            ListBoxItem newItem = new ListBoxItem()
+            {
+                Content = noteText
+            };
+            UiDetails.Items.Add(newItem);
+        }
+
+        //private void UiFirstButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Button newButton = new Button() { Content = "123456" };
+        //    UiTestStack.Children.Add(newButton);
+        //}
     }
+
 }
